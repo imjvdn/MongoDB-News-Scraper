705e8a6b921e4832e3bd71497348903a0ca52077
Louella Landicho <louellalandicho@Louellas-MacBook-Pro.local>
2018-03-14 23:33:38 -0700

    fix controllers

--- models/Article.js
+++ models/Article.js
@@ -1,23 +1,30 @@
-var mongoose = require('mongoose');
-
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
 var Schema = mongoose.Schema;
 
+// Create article schema
 var ArticleSchema = new Schema({
+  // title is a required string
   title: {
     type: String,
     required: true
   },
+  // link is a required string
+  // todo save summary paragraph instead of link
   link: {
     type: String,
     required: true
   },
-  comment: [{
+  // Saves array of notes.
+  notes: [{
     type: Schema.Types.ObjectId,
-    ref: "Comment"
+    ref: "Note"
   }]
 });
 
+// Create the Article model with the ArticleSchema
 var Article = mongoose.model("Article", ArticleSchema);
 
-//export model
+// Export the model
 module.exports = Article;
